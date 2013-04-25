8f05d1b94effd862b338d2c994672579e9446f0d
Tomas Babej <tbabej@redhat.com>
2013-04-25 16:29:23 +0200

    Add unicode methods to models

--- base/models.py
+++ base/models.py
@@ -5,17 +5,28 @@ from django.db import models
 class Competition(models.Model):
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 
 class CompetitionUserRegistration(models.Model):
     competition = models.ForeignKey('Competition')
     user = models.ForeignKey('User')
     timestamp = models.DateTimeField()
 
+    def __unicode__(self):
+        return (self.user.__unicode__() + u" competes in " +
+               self.competition.__unicode__())
+
 
 class CompetitionOrgRegistration(models.Model):
     competition = models.ForeignKey('Competition')
     organizer = models.ForeignKey('Organizer')
     timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return (self.user.__unicode__() + u" organizes " +
+               self.competition.__unicode__())
 
 
 # User-related models
@@ -24,6 +35,9 @@ class User(models.Model):
     competes = models.ManyToManyField('Competition',
                                       through='CompetitionUserRegistration')
     solved = models.ManyToManyField('Problem')
+
+    def __unicode__(self):
+        return self.login
 
 
 class Organizer(User):
@@ -40,11 +54,18 @@ class Event(models.Model):
                                             through='EventOrgRegistration',
                                             related_name='organizers')
 
+    def __unicode__(self):
+        return self.name
+
 
 class EventUserRegistration(models.Model):
     event = models.ForeignKey('Event')
     user = models.ForeignKey('User')
     timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return (self.user.__unicode__() + u" goes to " +
+               self.event.__unicode__())
 
 
 class EventOrgRegistration(models.Model):
@@ -52,9 +73,16 @@ class EventOrgRegistration(models.Model):
     organizer = models.ForeignKey('Organizer')
     timestamp = models.DateTimeField()
 
+    def __unicode__(self):
+        return (self.user.__unicode__() + u" organizes " +
+               self.event.__unicode__())
+
 
 class Camp(Event):
     location = models.CharField(max_length=100)  # temporary placeholder
+
+    def __unicode__(self):
+        return self.location
 
 
 # Solution-related models
@@ -63,16 +91,27 @@ class UserSolution(models.Model):
     problem = models.ForeignKey('Problem')
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return (self.user.__unicode__() + u":'s solution of " +
+                self.problem.__unicode__())
+
 
 class OrgSolution(models.Model):
     organizer = models.ForeignKey('Organizer')
     problem = models.ForeignKey('Problem')
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return (self.user.__unicode__() + u":'s ideal solution of " +
+                self.problem.__unicode__())
+
 
 # Problem-related models
 class Problem(models.Model):
     text = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.text
 
 
 class ProblemSet(models.Model):
@@ -80,10 +119,16 @@ class ProblemSet(models.Model):
     leaflet = models.ForeignKey('Leaflet', blank=True, null=True)
     problems = models.ManyToManyField('Problem')
 
+    def __unicode__(self):
+        return u"RpoblemSet for " + self.competition.__unicode__()
+
 
 class Leaflet(models.Model):
     competition = models.ForeignKey('Competition')
     year = models.IntegerField()
+
+    def __unicode__(self):
+        return u"Leaflet for " + self.competition.__unicode__()
 
 
 # Content-related models
@@ -91,6 +136,12 @@ class Post(models.Model):
     organizer = models.ForeignKey('Organizer')
     title = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.title
+
 
 class Gallery(models.Model):
     name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
