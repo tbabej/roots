cc02339717f392b0750be3b9a74a6406d8c2122f
Tomas Babej <tbabej@redhat.com>
2013-04-29 18:55:05 +0200

    Expand Event's attributes and add a __doc__ field to each class in events/models.py

--- events/models.py
+++ events/models.py
@@ -4,7 +4,19 @@ from django.contrib import admin
 
 # Event-related models
 class Event(models.Model):
+    """
+    Event represents a simple event, that is opened to public. This can be
+    either a public presentation, or a public game.
+
+    Users are not invited, but can notify the organizer that they want to
+    participate. This relation is represented using EventUserRegistration.
+    """
+
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
     galerry = models.ForeignKey('posts.Gallery')
     registered_user = models.ManyToManyField('users.User',
                                              through='EventUserRegistration')
@@ -17,6 +29,10 @@ class Event(models.Model):
 
 
 class EventUserRegistration(models.Model):
+    """
+    Represents a user's registration to the event.'
+    """
+
     event = models.ForeignKey('events.Event')
     user = models.ForeignKey('users.User')
     timestamp = models.DateTimeField()
@@ -27,6 +43,12 @@ class EventUserRegistration(models.Model):
 
 
 class EventOrgRegistration(models.Model):
+    """
+    Represents a organizer's registration to the event. This is merely for them
+    to let everybody know that they will be available (if help with organization
+    is needed).
+    """
+
     event = models.ForeignKey('events.Event')
     organizer = models.ForeignKey('users.Organizer')
     timestamp = models.DateTimeField()
@@ -37,6 +59,15 @@ class EventOrgRegistration(models.Model):
 
 
 class Camp(Event):
+    """
+    This class is called Camp from historical reasons. It is supposed to model
+    an workshop that is open only to the best of the competitors in the given
+    competition's season.
+
+    User's have to be invited to be able to register. Usually more users than
+    the capacity allows are invited, some are substitutes.'
+    """
+
     location = models.CharField(max_length=100)  # temporary placeholder
 
     def __unicode__(self):
