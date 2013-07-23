9d5b17e375b7f0b6b1273585a7427b0c3ca77535
Tomas Babej <tbabej@redhat.com>
2013-07-23 06:18:43 +0200

    Create basic explicit class-based views

--- base/views.py
+++ base/views.py
@@ -1,1 +1,28 @@
 # Create your views here.
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+class BaseView(TemplateView):
+
+    template_name = "base/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
+
+
+class SecureBaseView(BaseView):
+
+    template_name = "base/base.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(BaseView, self).dispatch(*args, **kwargs)
+
+
+class IndexView(TemplateView):
+
+    template_name = "base/index.html"
--- roots/urls.py
+++ roots/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from base.views import IndexView
 
 admin.autodiscover()
 
@@ -18,5 +19,5 @@ urlpatterns = patterns('',
     url(r'^accounts/profile/', TemplateView.as_view(
                                template_name='profile.html')),
     url(r'^problems/', include('problems.urls')),
-    url(r'^$', TemplateView.as_view(template_name='base/index.html'))
+    url(r'^$', IndexView.as_view())
 )
