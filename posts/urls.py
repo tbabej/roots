d931597d4b5a6ffd525b3c5d9c25a1075f15c767
Tomas Babej <tbabej@redhat.com>
2013-08-04 18:38:45 +0200

    Create a separate menu template

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from posts.views import PostListView
 
 urlpatterns = patterns('',
-    url(r'^$', PostListView.as_view())
+    url(r'^$', PostListView.as_view(), name="posts_list")
 )
