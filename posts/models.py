df83660248e39c51fd035aa212f06f9542b01620
Tomas Babej <tbabej@redhat.com>
2013-08-15 01:18:44 +0200

    Remove Gallery model

--- posts/models.py
+++ posts/models.py
@@ -23,22 +23,5 @@ class Post(models.Model):
         verbose_name_plural = 'Posts'
 
 
-@with_author
-@with_timestamp
-class Gallery(models.Model):
-    '''
-    Represents a gallery of photos. This can be restricted to certain
-    competition or event.
-    '''
-
-    name = models.CharField(max_length=50)
-    competition = models.ForeignKey('competitions.Competition')
-    event = models.ForeignKey('events.Event')
-    date = models.DateTimeField()
-
-    def __unicode__(self):
-        return self.name
-
 # Register to the admin site
 admin.site.register(Post)
-admin.site.register(Gallery)
