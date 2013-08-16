497451ee060b1dd7655ad648f4026633e6e3318c
Tomas Babej <tbabej@redhat.com>
2013-08-16 12:11:04 +0200

    Add capability to upload Leaflets

--- leaflets/models.py
+++ leaflets/models.py
@@ -1,3 +1,5 @@
+import reversion
+
 from django.db import models
 from django.contrib import admin
 
@@ -7,9 +9,22 @@ class Leaflet(models.Model):
     Represents a given (generated) leaflet.
     '''
 
+    def generate_name(self, filename):
+        return "leaflets/{competition}-{year}-{issue}.pdf"\
+               .format(competition=self.competition,
+                       year=self.year,
+                       issue=self.issue)
+
+    def __unicode__(self):
+        return "{competition}-{year}-{issue}"\
+               .format(competition=self.competition,
+                       year=self.year,
+                       issue=self.issue)
+
     competition = models.ForeignKey('competitions.Competition')
     year = models.IntegerField()
     issue = models.IntegerField()
+    leaflet = models.FileField(upload_to=generate_name)
 
     # Fields added via foreign keys:
 
@@ -18,14 +33,19 @@ class Leaflet(models.Model):
     # TODO: more than one problemset can point to given leaflet, is that a
     #       problem?
 
-    def __unicode__(self):
-        return u"Leaflet for " + self.competition.__unicode__()
-
     class Meta:
-        ordering = ['competition', 'year', 'issue']
+        ordering = ['competition', '-year', 'issue']
         verbose_name = 'Leaflet'
         verbose_name_plural = 'Leaflets'
 
 
+class LeafletAdmin(reversion.VersionAdmin):
+
+    list_display = ('competition',
+                    'year',
+                    'issue',
+                    )
+
+
 # Register to the admin site
-admin.site.register(Leaflet)
+admin.site.register(Leaflet, LeafletAdmin)
