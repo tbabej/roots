c7ee258285eef4a35437e576a81eee6a06d97d1e
Tomas Babej <tbabej@redhat.com>
2013-09-05 00:16:49 +0200

    Add MediaRemovalMixin for models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.conf import settings
+
+
+class MediaRemovalMixin(object):
+    """
+    Removes all files associated with the model, as returned by the
+    get_media_files() method.
+    """
+
+    # Models that use this mixin need to override this method
+    def get_media_files(self):
+        return
+
+    def delete(self):
+        for media_file in self.get_media_files():
+            path = settings.PROJECT_ROOT.child(media_file)
+            os.remove(path)
+
+        return super(MediaRemovalMixin, self).delete()
