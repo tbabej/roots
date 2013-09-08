04c65e87860397d37a978aa931a22acd84a70d38
Tomas Babej <tbabej@redhat.com>
2013-09-08 14:23:17 +0200

    events: Create EventUserRegisterView

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls import patterns, url
-from events.views import EventListView
+from events.views import EventListView, EventUserRegisterView
 
 urlpatterns = patterns('',
-    url(r'^$', EventListView.as_view(), name="events_event_list")
+    url(
+        r'^$',
+        EventListView.as_view(),
+        name="events_event_list"
+    ),
+    url(
+        r'register/(?P<event_id>\d+)/user/$',
+        EventUserRegisterView.as_view(),
+        name="events_event_user_registration"
+    ),
+    url(
+        r'register/(?P<event_id>\d+)/organizer/$',
+        EventUserRegisterView.as_view(),
+        name="events_event_user_registration"
+    ),
 )
--- events/views.py
+++ events/views.py
@@ -1,8 +1,41 @@
 from django.views.generic.list import ListView
-from events.models import Event
+from base.views import RedirectBackView
+
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from base.util import class_view_decorator
+
+from .models import Event, EventUserRegistration
 
 
 class EventListView(ListView):
 
     model = Event
     context_object_name = 'events'
+
+
+@class_view_decorator(login_required)
+class EventUserRegisterView(RedirectBackView):
+
+    default_return_view = 'events_event_list'
+
+    def dispatch(self, request, *args, **kwargs):
+        event = Event.objects.get(pk=kwargs['event_id'])
+
+        if event.registration_open():
+            registration = EventUserRegistration(user=request.user, event=event)
+            registration.save()
+
+            message = 'Successfully registered to the %s' % event
+            messages.add_message(request, messages.INFO, message)
+        else:
+            message = 'Registration to the %s is not open.' % event
+            messages.add_message(request, messages.ERROR, message)
+
+        return super(EventUserRegisterView, self).dispatch(request,
+                                                           *args, **kwargs)
+
+
+
+
