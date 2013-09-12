2766e8797515497e5569b31696416db68641c9b4
Tomas Babej <tbabej@redhat.com>
2013-09-12 17:47:54 +0200

    base: Extend MediaRemovalMixin to move media files on updates

--- base/models.py
+++ base/models.py
@@ -13,11 +13,28 @@ class MediaRemovalMixin(object):
     def get_media_files(self):
         return
 
-    def delete(self):
+    def delete(self, *args, **kwargs):
         for media_file in self.get_media_files():
             path = settings.MEDIA_ROOT + media_file
 
             if os.path.exists(path):
                 os.remove(path)
 
-        return super(MediaRemovalMixin, self).delete()
+        return super(MediaRemovalMixin, self).delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            # Primary key exists, object is being edited
+            old_object = self.__class__.objects.get(pk=self.pk)
+            path_pairs = zip(old_object.get_media_files(),
+                             self.get_media_files())
+
+            # Move each associated file to its new location
+            for (old_path, new_path) in path_pairs:
+                full_old_path = settings.MEDIA_ROOT + old_path
+                full_new_path = settings.MEDIA_ROOT + new_path
+
+                if old_path != new_path and os.path.exists(full_old_path):
+                    os.rename(full_old_path, full_new_path)
+
+        return super(MediaRemovalMixin, self).save(*args, **kwargs)
