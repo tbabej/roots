484ffb7d2b87b51de7d3c106b1266f1b37bb2ec7
Tomas Babej <tomasbabej@gmail.com>
2014-03-23 22:48:57 +0100

    posts: Add PostDetailView

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = patterns('',
-    url(r'^$', PostListView.as_view(), name="posts_post_list")
+    url(r'^$', PostListView.as_view(), name="posts_post_list"),
+    url(r'^(?P<slug>[\w-]+)/$', PostDetailView.as_view(),
+        name="posts_post_detail")
 )
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Post
 
@@ -7,3 +8,8 @@ class PostListView(ListView):
 
     model = Post
     context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+
+    model = Post
+    context_object_name = 'post'
