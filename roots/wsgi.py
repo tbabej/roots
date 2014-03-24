2d8aa464c71d465dd45defecffe891b8ea87a99e
Tomas Babej <tomasbabej@gmail.com>
2014-03-24 20:21:51 +0100

    Update wsgi to this server specific configuration

--- roots/wsgi.py
+++ roots/wsgi.py
@@ -14,8 +14,18 @@ framework.
 
 """
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roots.settings")
+
+paths = [
+    '/var/www/roots-env/roots',
+    '/var/www/roots-env/lib/python2.7/site-packages'
+]
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
