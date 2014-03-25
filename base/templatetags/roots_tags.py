b74438d25175c9ee03016c5bbeeb14f8c86f1f48
Tomas Babej <tomasbabej@gmail.com>
2014-03-25 21:13:05 +0100

    roots_tags: Add {% define %} template tag
    Adds {% define %} template tag that is used for defining a template
    variable with multiline content that is added to the template's context.
    
    Within the template tag the content can also contain other template code,
    including blocks which can be extended by the templates that are inheriting
    from this template.

--- base/templatetags/roots_tags.py
+++ base/templatetags/roots_tags.py
@@ -3,7 +3,6 @@ from django.core.urlresolvers import reverse
 
 register = template.Library()
 
-
 @register.simple_tag
 def url_active(request, urls):
     if request.path in (reverse(url) for url in urls.split()):
@@ -25,3 +24,42 @@ def remove_uncomplete_latex(text):
     else:
         return text
 
+class DefineNode(template.Node):
+    def __init__(self, name, nodelist):
+        self.name = name
+        self.nodelist = nodelist
+
+    def __repr__(self):
+        return "<DefineNode>"
+
+    def render(self, context):
+        context[self.name] = self.nodelist.render(context)
+        return ''
+
+@register.tag
+def define(parser, token):
+    """
+    Adds a name to the context for referencing an arbitrarily defined block
+    of template code.
+
+    For example:
+
+        {% define my_block %}
+        This is the content.
+        {% enddefine %}
+
+    Now anywhere in the template:
+
+        {{ my_block }}
+    """
+
+    bits = list(token.split_contents())
+
+    if len(bits) != 2:
+        raise TemplateSyntaxError("Expected format is: {% define variable %}")
+
+    name = bits[1]
+    nodelist = parser.parse(('enddefine',))
+    parser.delete_first_token()
+
+    return DefineNode(name, nodelist)
