3752a1b812528727b98aa8d8ec94d64c6e9c75b9
Tomas Babej <tomasbabej@gmail.com>
2014-03-27 20:04:52 +0100

    problems: Remove ProblemList view from urls and menu

--- problems/urls.py
+++ problems/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from problems.views import ProblemListView, ProblemDetailView
 
 urlpatterns = patterns('',
-    url(r'^$', ProblemListView.as_view(), name='problems_problem_list'),
+#    url(r'^$', ProblemListView.as_view(), name='problems_problem_list'),
     url(r'^(?P<pk>\d+)/$', ProblemDetailView.as_view(),
         name='problems_problem_detail')
 )
