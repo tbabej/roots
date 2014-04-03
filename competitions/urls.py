e956e2258037e698adcf86a57085bdb2aa7576e5
Tomas Babej <tomasbabej@gmail.com>
2014-04-03 07:55:24 +0200

    competitions: Correct url of the SeasonDetailView

--- competitions/urls.py
+++ competitions/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     url(r'^registration/successful/', TemplateView.as_view(
         template_name='competitions/competition_registration_successful.html'),
         name='competitions_competition_register_success'),
-    url(r'^series/(?P<pk>\d+)$', SeasonDetailView.as_view(),
+    url(r'^season/(?P<pk>\d+)$', SeasonDetailView.as_view(),
         name='competitions_season_detail'),
 )
