ee95cae5909607040cb9366fd948e625770308cb
Tomas Babej <tomasbabej@gmail.com>
2014-04-13 13:34:06 +0200

    leaflets: Fix indentation in urls.py

--- leaflets/urls.py
+++ leaflets/urls.py
@@ -5,6 +5,6 @@ from .views import LeafletListView, LeafletCompetitionListView
 urlpatterns = patterns('',
     url(r'^$', LeafletListView.as_view(), name="leaflets_leaflet_list"),
     url(r'^competitions/(?P<competition_id>\d+)/$',
-    LeafletCompetitionListView.as_view(),
-    name="leaflets_leaflet_competition_list"),
+        LeafletCompetitionListView.as_view(),
+        name="leaflets_leaflet_competition_list"),
     )
