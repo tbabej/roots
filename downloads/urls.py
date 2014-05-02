59ce3ca9c1572dcf71aa5de5cdb354def594a36c
Tomas Babej <tomasbabej@gmail.com>
2014-05-02 09:55:47 +0200

    downloads: Remove unnecessary usage of functools.partial

--- downloads/urls.py
+++ downloads/urls.py
@@ -6,8 +6,7 @@ from problems.models import UserSolution
 from .views import download_protected_file
 
 urlpatterns = patterns('',
-    url(r'solutions/(?P<path>.*)$', partial(download_protected_file,
-                                            path_prefix='solutions/',
-                                            model_class=UserSolution),
+    url(r'solutions/(?P<path>.*)$', download_protected_file,
+        dict(path_prefix='solutions/', model_class=UserSolution),
         name='download_solution'),
 )
