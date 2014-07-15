9dc3ce8b1ae79fcb4578fbb9109c9da57108ca50
Tomas Babej <tomasbabej@gmail.com>
2014-07-15 07:08:34 +0200

    profiles.models: Localize hardcoded strings

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
 
 # User-related models
@@ -26,29 +27,61 @@ class UserProfile(models.Model):
     # last_login
     # date_joined
 
-    user = models.OneToOneField('auth.User')
+    user = models.OneToOneField('auth.User',
+                                verbose_name=_('user'))
 
     # personal info
-    date_of_birth = models.DateTimeField(blank=True, null=True)
-    sex = models.CharField(max_length=1, blank=True, null=True,
-                           choices=(('M', 'male'), ('F', 'female')))
-    social_security_number = models.CharField(max_length=11, blank=True,
-                                              null=True)
-    state_id_number = models.CharField(max_length=8, blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True,
+                                         null=True,
+                                         verbose_name=_('date of birth'))
+    sex = models.CharField(max_length=1,
+                           blank=True,
+                           null=True,
+                           choices=(('M', 'male'), ('F', 'female')),
+                           verbose_name=_('sex'))
+    social_security_number = models.CharField(
+                                 max_length=11,
+                                 blank=True,
+                                 null=True,
+                                 verbose_name=_('social security number'))
+    state_id_number = models.CharField(max_length=8,
+                                       blank=True,
+                                       null=True,
+                                       verbose_name=_('ID number'))
 
     competes = models.ManyToManyField('competitions.Competition',
                     through='competitions.CompetitionUserRegistration',
-                    blank=True)
+                    blank=True,
+                    verbose_name=_('competes in'))
 
     # address information
-    phone_number = models.CharField(max_length=30, blank=True, null=True)
-    parent_phone_number = models.CharField(max_length=30, blank=True, null=True)
-    address = models.ForeignKey('schools.Address', blank=True, null=True)
+    phone_number = models.CharField(max_length=30,
+                                    blank=True,
+                                    null=True,
+                                    verbose_name=_('phone number'))
+    parent_phone_number = models.CharField(
+                              max_length=30,
+                              blank=True,
+                              null=True,
+                              verbose_name=_('parent phone number'))
+    address = models.ForeignKey('schools.Address',
+                                blank=True,
+                                null=True,
+                                verbose_name=_('address'))
 
     # school related info
-    school = models.ForeignKey('schools.School', blank=True, null=True)
-    school_class = models.CharField(max_length=20, blank=True, null=True)
-    classlevel = models.CharField(max_length=2, blank=True, null=True,
+    school = models.ForeignKey('schools.School',
+                               blank=True,
+                               null=True,
+                               verbose_name=_('school'))
+    school_class = models.CharField(max_length=20,
+                                    blank=True,
+                                    null=True,
+                                    verbose_name=_('school class'))
+    classlevel = models.CharField(max_length=2,
+                                  blank=True,
+                                  null=True,
+                                  verbose_name=_('class level'),
                                   choices=(('Z2', 'Z2'),
                                            ('Z3', 'Z3'),
                                            ('Z4', 'Z4'),
@@ -76,15 +109,15 @@ class UserProfile(models.Model):
     #  organizer
 
     def __unicode__(self):
-        return self.user.username + "'s user profile"
+        return _("user profile: {user}").format(user=self.user.username)
 
     def organizes_competitions(self):
         return [registration.competition for registration in
                 self.competitionorgregistration_set.filter(approved=True)]
 
     class Meta:
-        verbose_name = 'User profile'
-        verbose_name_plural = 'User profiles'
+        verbose_name = _('user profile')
+        verbose_name_plural = _('user profiles')
 
 
 class OrganizerProfile(models.Model):
@@ -94,8 +127,12 @@ class OrganizerProfile(models.Model):
     Organizer can organize multiple competitions or events.
     '''
 
-    user = models.OneToOneField('auth.User')
-    motto = models.CharField(max_length=250, blank=True, null=True)
+    user = models.OneToOneField('auth.User',
+                                verbose_name=_('user'))
+    motto = models.CharField(max_length=250,
+                             blank=True,
+                             null=True,
+                             verbose_name=_('motto'))
 
     # TODO: there are 2 data descriptors added via many-to-many relationship
     #       in this case it's organized_event_set (custom name due to
@@ -117,11 +154,11 @@ class OrganizerProfile(models.Model):
     #  user_ptr
 
     def __unicode__(self):
-        return self.user.username + "'s organizer profile"
+        return _("organizer profile: {user}").format(user=self.user.username)
 
     class Meta:
-        verbose_name = 'Organizer profile'
-        verbose_name_plural = 'Organizer profiles'
+        verbose_name = _('organizer profile')
+        verbose_name_plural = _('organizer profiles')
 
 
 @receiver(post_save, sender=User)
