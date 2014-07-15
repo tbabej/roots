c1524c2fedce885f5bb44452e863373f6896b9e8
Tomas Babej <tomasbabej@gmail.com>
2014-07-15 06:35:20 +0200

    posts.models: Localize hardcoded strings

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from base.util import with_author, with_timestamp
 
@@ -12,15 +13,18 @@ class Post(models.Model):
     or can be general.
     '''
 
-    title = models.CharField(max_length=100)
-    competition = models.ForeignKey('competitions.Competition', blank=True,
-                                    null=True)
-    text = models.TextField()
-    slug = models.SlugField()
+    title = models.CharField(max_length=100,
+                             verbose_name=_('title'))
+    competition = models.ForeignKey('competitions.Competition',
+                                    blank=True,
+                                    null=True,
+                                    verbose_name=_('competition'))
+    text = models.TextField(verbose_name=_('text'))
+    slug = models.SlugField(verbose_name=_('slug'))
 
     def __unicode__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Post'
-        verbose_name_plural = 'Posts'
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
