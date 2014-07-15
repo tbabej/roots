8664adc049e28fb35a88b9e77bea0e6be7871c2a
Tomas Babej <tomasbabej@gmail.com>
2014-07-15 07:08:51 +0200

    schools.models: Localize hardcoded strings

--- schools/models.py
+++ schools/models.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Address(models.Model):
@@ -7,18 +8,18 @@ class Address(models.Model):
     Represents a postal address.
     """
 
-    street = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=100, blank=True)
-    postal_number = models.CharField(max_length=10, blank=True)
-    region = models.CharField(max_length=30, blank=True,
-                              choices=(('KE', 'Kosicky'),
-                                       ('PR', 'Presovsky'),
-                                       ('BB', 'Banskobystricky'),
-                                       ('BA', 'Bratislavsky'),
-                                       ('ZI', 'Zilinsky'),
-                                       ('TC', 'Trenciansky'),
-                                       ('TV', 'Trnavsky'),
-                                       ('NI', 'Nitriansky')))
+    street = models.CharField(max_length=200,
+                              blank=True,
+                              verbose_name=_('street'))
+    city = models.CharField(max_length=100,
+                            blank=True,
+                            verbose_name=_('city'))
+    postal_number = models.CharField(max_length=10,
+                                     blank=True,
+                                     verbose_name=_('zip code'))
+    region = models.CharField(max_length=30,
+                              blank=True,
+                              verbose_name=_('region'))
 
     def __unicode__(self):
         template = "{street}, {city}, {postal}, {region}"
@@ -35,8 +36,11 @@ class School(models.Model):
     lyceum or torture room.
     """
 
-    name = models.CharField(max_length=150)
-    address = models.ForeignKey('schools.Address', blank=True)
+    name = models.CharField(max_length=150,
+                            verbose_name=_('name'))
+    address = models.ForeignKey('schools.Address',
+                                blank=True,
+                                verbose_name=_('address'))
 
     def __unicode__(self):
         return self.name
