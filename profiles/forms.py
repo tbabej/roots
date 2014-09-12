4d1c8620ec9fcadf39c87f6ab794f82da23be91c
Tomas Babej <tomasbabej@gmail.com>
2014-09-12 18:41:00 +0200

    profiles: Redesign the UpdateProfile view and associated forms and template

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,72 @@
-from django.forms import ModelForm
+from django import forms
+from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import string_concat
+
+from crispy_forms.helper import FormHelper
+
+from django.contrib.auth.models import User
 
 from .models import UserProfile
 
 
-class UserProfileForm(ModelForm):
+class UsernameForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UsernameForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
+
+
+class UserProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ('user', 'address')
+        help_texts = {
+            'school': string_concat(
+                    _('Pick the school from the list. '
+                      'If your school is not available, please, '),
+                    '<a href="',
+                    reverse_lazy('schools_school_create'),
+                    _('">add it.</a>')
+                ),
+            'phone_number': _('Needed to contact you in case you qualified '
+                              'to attend a camp.'),
+            'school_class': _('Name of your class in the school, such '
+                              'as "4.A".'),
+            'parent_phone_number': _("Mobile phone number of the parent, or "
+                                     "of any other lawful representative.")
+        }
+
+EVENT_ONLY_FIELDS = ('date_of_birth', 'parent_phone_number', 'sex')
+
+
+class UserProfileBasicForm(UserProfileForm):
+    """
+    Form containing the fields considered necessary for user participating
+    in the competition.
+    """
+
+    class Meta(UserProfileForm.Meta):
+            exclude = (UserProfileForm.Meta.exclude +
+                       EVENT_ONLY_FIELDS)
+
+
+class UserProfileEventForm(UserProfileForm):
+    """
+    Form containing the fields considered necessary for user attending
+    camps.
+    """
+
+    class Meta(UserProfileForm.Meta):
+            fields = EVENT_ONLY_FIELDS
--- profiles/views.py
+++ profiles/views.py
@@ -2,28 +2,98 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from django.views.generic.edit import UpdateView
+
+from django.utils.translation import ugettext_lazy as _
+
+from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 
 
 from base.util import class_view_decorator
 
-from .forms import UserProfileForm
+from schools.forms import AddressForm
+from .forms import UserProfileBasicForm, UserProfileEventForm, UsernameForm
+
 from .models import UserProfile
 
 
 @class_view_decorator(login_required)
-class UserProfileUpdate(UpdateView):
+class UserProfileUpdate(TemplateView):
 
-    form_class = UserProfileForm
-    success_url = '/profiles/update'
+    template_name = 'profiles/userprofile_form.html'
 
-    def get_object(self):
-        return UserProfile.objects.all().filter(user=self.request.user)[0]
+    basic_profile_form = None
+    event_profile_form = None
+    address_form = None
+    name_form = None
 
-    def form_valid(self, form):
-        messages.add_message(self.request, messages.INFO, 'Hello world.')
-        return super(UserProfileUpdate, self).form_valid(form)
+    @property
+    def forms(self):
+        return (
+            self.basic_profile_form,
+            self.event_profile_form,
+            self.address_form,
+            self.name_form
+        )
+
+    def set_forms(self):
+        if all(form is not None for form in self.forms):
+            return
+
+        if self.request.POST:
+            self.basic_profile_form = UserProfileBasicForm(self.request.POST,
+                                   instance=self.request.user.userprofile)
+            self.event_profile_form = UserProfileEventForm(self.request.POST,
+                                   instance=self.request.user.userprofile)
+            self.address_form = AddressForm(self.request.POST,
+                               instance=self.request.user.userprofile.address)
+            self.name_form = UsernameForm(self.request.POST,
+                               instance=self.request.user)
+        else:
+            self.basic_profile_form = UserProfileBasicForm(
+                               instance=self.request.user.userprofile)
+            self.event_profile_form = UserProfileEventForm(
+                               instance=self.request.user.userprofile)
+            self.address_form = AddressForm(
+                               instance=self.request.user.userprofile.address)
+            self.name_form = UsernameForm(instance=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileUpdate, self).get_context_data(**kwargs)
+
+        self.set_forms()
+
+        context_forms = {
+            'basic_profile_form': self.basic_profile_form,
+            'event_profile_form': self.event_profile_form,
+            'address_form': self.address_form,
+            'name_form': self.name_form,
+            }
+
+        context.update(context_forms)
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.set_forms()
+
+        forms_valid = all(form.is_valid() for form in self.forms)
+
+        if forms_valid:
+            self.name_form.save()
+            self.event_profile_form.save()
+            address = self.address_form.save()
+            profile = self.basic_profile_form.save(commit=False)
+            profile.address = address
+            profile.save()
+
+        if any(form.has_changed() for form in self.forms):
+            if forms_valid:
+                messages.success(request, _("Profile successfully updated."))
+        else:
+            messages.info(request, _("Nothing has changed."))
+
+        return self.get(request)
 
 
 class UserProfileDetail(DetailView):
