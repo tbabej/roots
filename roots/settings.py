1a30839aefa1451e474ac66e828cdb3505eeeaba
Tomas Babej <tomasbabej@gmail.com>
2014-09-14 21:34:06 +0200

    roots: Enable template caching

--- roots/settings.py
+++ roots/settings.py
@@ -54,10 +54,13 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 
 # List of callables that know how to import templates from various sources.
+
+# Enable template caching by default
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
