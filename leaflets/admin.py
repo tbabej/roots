6a14aa5762da472d2cc41541a1e6ebad4b7593d1
Tomas Babej <tomasbabej@gmail.com>
2014-11-20 20:50:31 +0100

    admin: Extend Leaflet admin

--- leaflets/admin.py
+++ leaflets/admin.py
@@ -13,10 +13,36 @@ class LeafletAdmin(RestrictedCompetitionAdminMixin,
                    DownloadMediaFilesMixin,
                    reversion.VersionAdmin):
 
-    list_display = ('competition',
-                    'year',
-                    'issue',
-                    )
+    list_display = (
+        'competition',
+        'year',
+        'issue',
+    )
+
+    list_filter = (
+        'competition',
+    )
+
+    search_fields = (
+        'competition',
+        'user__name',
+        'user__first_name',
+        'user__last_name'
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('competition', 'year', 'issue', 'leaflet')
+        }),
+    )
+
+    editonly_fieldsets = (
+        ('Details', {
+            'classes': ('grp-collapse', 'grp-closed'),
+            'fields': ('added_by', 'added_at', 'modified_by', 'modified_at')
+        }),
+    )
+
 
 # Register to the admin site
 admin.site.register(Leaflet, LeafletAdmin)
