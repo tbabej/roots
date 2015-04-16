326626eb118d40ff705e68ee39221efd8d34e02b
Tomas Babej <tomasbabej@gmail.com>
2015-04-16 02:12:01 +0200

    storage: Perform a sanity check after removal of the old version of the file in the OverwriteFileSystemStorage

--- base/storage.py
+++ base/storage.py
@@ -9,6 +9,11 @@ class OverwriteFileSystemStorage(FileSystemStorage):
     def _save(self, name, content):
         if self.exists(name):
             self.delete(name)
+        if self.exists(name):
+            raise Exception("Unable to delete old version of the file: %s"
+                            "Please ensure that process running the app "
+                            "can properly change the contents of the "
+                            "target directory." % name)
         return super(OverwriteFileSystemStorage, self)._save(name, content)
 
     def get_available_name(self, name):
