eda534d50add9eeee3e4b9998adac6807464f2fa
Tomas Babej <tomasbabej@gmail.com>
2015-07-14 07:34:54 +0200

    admin_custom: Add initial dashboard index view

--- admin_custom/urls.py
+++ admin_custom/urls.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
+from django.utils.translation import ugettext_lazy as _
+
+from .views import DashboardIndexView
 
 urlpatterns = patterns('',
+    url(_(r'^$'), DashboardIndexView.as_view(),
+        name="admin_dashboard_index"),
 )
--- admin_custom/views.py
+++ admin_custom/views.py
@@ -1,1 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from .dashboard import IndexDashboard
+
+class DashboardIndexView(TemplateView):
+    template_name = "admin_custom/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DashboardIndexView, self).get_context_data(*args,
+                                                                   **kwargs)
+        context['dashboard'] = IndexDashboard()
+        return context
