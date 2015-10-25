d0beba6fc9c0a0411013954ea61ebdab79428980
Tomas Babej <tomasbabej@gmail.com>
2015-10-25 17:48:25 +0100

    settings: Replace PROJECT_ROOT with BASE_DIR
    BASE_DIR seems to be the generated name for the base directory since
    Django 1.6.

--- roots/settings.py
+++ roots/settings.py
@@ -206,27 +206,26 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 
 # Get the projet root
 settings_dir = os.path.dirname(os.path.realpath(__file__))
-PROJECT_ROOT = os.path.dirname(settings_dir) + '/'
-BASE_DIR = PROJECT_ROOT
+BASE_DIR = os.path.dirname(settings_dir) + '/'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or
     # "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_ROOT + 'templates_custom/',
-    PROJECT_ROOT + 'templates/',
+    BASE_DIR + 'templates_custom/',
+    BASE_DIR + 'templates/',
 )
 
 LOCALE_PATHS = (
-    PROJECT_ROOT + 'locale/',
+    BASE_DIR + 'locale/',
     # Use this directory for localized string in your custom templates
-    PROJECT_ROOT + 'templates_custom/locale/'
+    BASE_DIR + 'templates_custom/locale/'
 )
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_ROOT + 'media/'
+MEDIA_ROOT = BASE_DIR + 'media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -237,7 +236,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_ROOT + 'static/'
+STATIC_ROOT = BASE_DIR + 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -274,7 +273,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # Django-sendfile related settings
 SENDFILE_DIR = 'protected/'
-SENDFILE_ROOT = os.path.join(PROJECT_ROOT, SENDFILE_DIR)
+SENDFILE_ROOT = os.path.join(BASE_DIR, SENDFILE_DIR)
 SENDFILE_URL = '/' + SENDFILE_DIR
 
 # Roots-related settings
